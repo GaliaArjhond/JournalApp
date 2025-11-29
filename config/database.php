4c192122025b5b054d3f41b7ffd88178557f3b70
GaliaArjhond <galiaarjhond0@gmail.com>
2025-11-29 15:35:07 +0800

    add login

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'journalApp';
+$username = 'root';
+$password = 'admin';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit;
+}
